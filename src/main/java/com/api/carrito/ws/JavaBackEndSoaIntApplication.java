fcb1881912104316caeaab910327e8d19211f5ac
Jhonatan Mallqui Diaz - EMP.PROV. - EVERIS <jhonatanmallquid@bcp.com.pe>
2020-09-26 14:55:01 -0500

    add files

---
+++ src/main/java/com/api/carrito/ws/JavaBackEndSoaIntApplication.java
@@ -0,0 +1,13 @@
+package com.api.carrito.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackEndSoaIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackEndSoaIntApplication.class, args);
+	}
+
+}
